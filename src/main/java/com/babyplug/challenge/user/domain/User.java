7434b07a85a66a37000aa6f907ad4c81f7292491
babyplug <poramin.le.58@ubu.ac.th>
2020-09-08 13:28:27 +0700

    create Entity and Repository of User

---
+++ src/main/java/com/babyplug/challenge/user/domain/User.java
@@ -0,0 +1,111 @@
+package com.babyplug.challenge.user.domain;
+
+import com.babyplug.challenge.core.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String displayName;
+    private Long age;
+
+    private String username;
+    @JsonIgnore
+    private String password;
+
+    @JsonIgnore
+    private Boolean deleted;
+
+    private Long createdBy;
+    private Long updatedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
---
+++ src/main/java/com/babyplug/challenge/user/domain/UserRepository.java
@@ -0,0 +1,22 @@
+package com.babyplug.challenge.user.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Page<User> findAll(Specification<User> specification, Pageable pageable);
+
+    List<User> findAll(Specification<User> specification);
+
+    List<User> findAll(Specification<User> specification, Sort sort);
+
+    Optional<User> findByIdAndDeleted(Long id, boolean deleted);
+
+}
