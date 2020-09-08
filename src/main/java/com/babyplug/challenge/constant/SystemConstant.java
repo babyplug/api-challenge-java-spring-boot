e8cc174b852294817edf5f53f92d59c8c4058cd8
babyplug <poramin.le.58@ubu.ac.th>
2020-09-08 13:12:54 +0700

    create SystemConstant

---
+++ src/main/java/com/babyplug/challenge/constant/SystemConstant.java
@@ -0,0 +1,17 @@
+package com.babyplug.challenge.constant;
+
+public class SystemConstant {
+
+    public static final int PAGE_SIZE = 50;
+    public static final int PAGE_MAX = Integer.MAX_VALUE;
+
+    // Time zone
+    public static final String TIME_ZONE = "GMT+7:00";
+    public static final String ZONE_TH = "Asia/Bangkok";
+
+    //Rest path url
+    public static final String IMAGE_PROFILE_URL = "public/images/";
+    public static final String ATTACHMENTS_URL = "/public/attachments/";
+    public static final String PRIVATE_ATTACHMENTS_URL = "/attachments/";
+
+}
