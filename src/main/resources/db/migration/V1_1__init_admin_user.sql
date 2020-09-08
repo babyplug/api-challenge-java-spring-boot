8168ba6837bb9d8860dc259f2eb17acad281fe07
babyplug <poramin.le.58@ubu.ac.th>
2020-09-08 14:08:57 +0700

    add migration to initial admin user

---
+++ src/main/resources/db/migration/V1_1__init_admin_user.sql
@@ -0,0 +1,2 @@
+-- PASSWORD: admin@2020
+INSERT INTO user(first_name, last_name, display_name, username, password, deleted, created_time, updated_time) VALUES ('admin', 'admin', 'admin', 'admin', '$2a$11$tafY8pkricu3tVdYwSF9Ae.000THgqBrw67xG6yPpqUFs6FKj.AO.', 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
